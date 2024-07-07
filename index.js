35df0c1d6d7ab17da103a3316ce14321012446d0
Abdulahad911 <a.a.khan6204@gmail.com>
2024-07-06 22:37:20 -0700

    changes

---
+++ index.js
@@ -0,0 +1,75 @@
+// //1. table
+// var num = +prompt("Please Enter your number");
+// for (let index = 0; index < 10; index++) {
+//    var multiply = num * index; 
+//   console.log(`${num}*${index}=${multiply}`);
+    
+// }
+
+
+//2. Array Element ka sum print kare
+// var user_arr = [];
+// var user_sum = 0;
+// for (let i = 0; i < 5 ; i++) {
+//     var user_input = +prompt("Enter your element");
+//     user_arr.push(user_input);
+//     user_sum += user_arr[i];
+    
+    
+// }
+// console.log(`array: ${user_arr}`)
+// console.log(`the sum of element: ${user_sum}`);
+
+
+
+// 3. ARRAY KA SUB SE BARA NUMBER PRINT KARE
+// var num_arr = [];  
+// var max = 0; 
+
+
+// for (let i = 0; i < 5; i++) {
+//     var user = +prompt("Enter your number"); 
+//     num_arr.push(user);  
+    
+//     if (num_arr[i] > max) {  
+//         max = num_arr[i]; 
+//     }
+// }
+
+// console.log(`Array of numbers: ${num_arr}`);  
+// console.log(`Maximum number entered: ${max}`);  
+    
+
+    
+
+// // 4. USER SE STRING LE AND USKO ULTA PRINT KARE
+
+//  let original = prompt("Enter your string");  
+// let reversed = ""; 
+
+
+// for (let i = original.length - 1; i >= 0; i--) { 
+//     reversed += original[i]; 
+// } 
+
+// console.log(reversed);  
+
+
+// 5. FACTORIAL
+
+// let number = +prompt("Enter a number to find its factorial");
+
+// let factorial = 1;
+
+
+// for (let i = 1; i <= number; i++) {
+//     factorial *= i;
+// }
+
+
+// console.log(`The factorial of ${number} is ${factorial}`);
+
+
+
+
+
